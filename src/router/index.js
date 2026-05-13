ecb58dfdc06eaee9d0ba66320969112bbfe6c422
Ryckie Wong <ricky600900@gmail.com>
2026-05-13 23:31:58 +0800

    first commit

---
+++ src/router/index.js
@@ -0,0 +1,51 @@
+import LoginView from '@/views/LoginView.vue'
+import RegisterView from '@/views/RegiterView.vue'
+import TodoListView from '@/views/TodoListView.vue'
+import { createRouter, createWebHistory } from 'vue-router'
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+
+  routes: [
+    {
+      path: '/',
+      redirect: '/login',
+    },
+
+    {
+      path: '/todoList',
+      name: 'todoList',
+      component: TodoListView,
+      meta: { requiresAuth: true },
+    },
+
+    {
+      path: '/login',
+      name: 'login',
+      component: LoginView,
+    },
+
+    {
+      path: '/register',
+      name: 'register',
+      component: RegisterView,
+    },
+  ],
+})
+
+// 路由守衛
+router.beforeEach((to, from) => {
+  const token = document.cookie.replace(
+    /(?:(?:^|.*;\s*)vue3-todolist-token\s*=\s*([^;]*).*$)|^.*$/,
+    '$1',
+  )
+
+  if (to.meta.requiresAuth && !token) {
+    return '/login'
+  }
+
+  if ((to.path === '/login' || to.path === '/register') && token) {
+    return '/todoList'
+  }
+})
+export default router
---
+++ src/views/TodoListView.vue
@@ -0,0 +1,44 @@
+<template>
+    <div id="todoListPage" class="bg-half">
+  <nav>
+    <h1><a href="#">ONLINE TODO LIST</a></h1>
+    <ul>
+      <li class="todo_sm"><a href="#"><span>Ryckie 的待辦</span></a></li>
+      <li><a href="#loginPage">登出</a></li>
+    </ul>
+  </nav>
+  <div class="conatiner todoListPage vhContainer">
+    <div class="todoList_Content">
+<TodoForm @add-todo="addTodo"></TodoForm>
+<TodoList v-if="todos.length" :todos ="todos" @remove-todo="removeTodo"></TodoList>
+
+<p v-else>尚無待辦事項</p>
+    </div>
+  </div>
+</div>
+</template>
+
+<script setup>
+import TodoForm from '@/components/TodoForm.vue';
+import TodoList from '@/components/TodoList.vue';
+import {ref} from 'vue';
+const todos = ref([
+   { id: 1, content: "買健康餐盒", status: false },
+   { id: 2, content: "去健身房一小時", status: true },
+])
+
+const addTodo =(content) =>{
+  if (content.trim() !== ''){
+    todos.value.push({
+      id: Date.now(),
+      content,
+      status:false,
+    })
+  } 
+}
+
+const removeTodo =(id) => {
+  todos.value = todos.value.filter((t) => t.id !== id)
+}
+
+</script>
